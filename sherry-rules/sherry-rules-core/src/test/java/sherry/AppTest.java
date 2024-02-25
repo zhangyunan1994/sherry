94142c09eba14ed40d1529d4efc3d3083f9fb534
yunan.zhang <zyndev@gmail.com>
2024-02-25 21:59:25 +0800

    first

---
+++ sherry-rules/sherry-rules-core/src/test/java/sherry/AppTest.java
@@ -0,0 +1,17 @@
+package sherry;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+  @Test
+  public void testApp() {
+    assertTrue(true);
+  }
+}
